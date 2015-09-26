644c0f30fed8b43b061e0ffafded7121a44e8aa0
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-27 01:29:40 +0800

    Create BaseSteering class.

---
+++ Assets/NPCs/BaseSteering.cs
@@ -0,0 +1,14 @@
+﻿public abstract class BaseSteering<T>
+{
+    private T npc;
+
+    public T NPC
+    {
+        get { return npc; }
+    }
+
+    protected BaseSteering(T npc)
+    {
+        this.npc = npc;
+    }
+}
