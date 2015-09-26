688435956e1426db34b68d672e9109bb37c94aca
talford2 <talford2@hotmail.com>
2015-09-27 02:42:09 +1000

    Cleaning up

--- Assets/Scripts/NPCs/Soldier/Soldier.cs
+++ Assets/Scripts/NPCs/Soldier/Soldier.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class Soldier : MonoBehaviour
 {
+	public BaseState<Soldier> State { get; set; }
+
 	void Start()
 	{
-
+		State = new SoldierIdleState(this);
 	}
 
 	void Update()
 	{
-
+		State.Update();
 	}
 }
---
+++ Assets/Scripts/NPCs/Soldier/SoldierIdleState.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoldierIdleState : BaseState<Soldier>
+{
+	public SoldierIdleState(Soldier npc) : base(npc)
+	{
+	}
+}
