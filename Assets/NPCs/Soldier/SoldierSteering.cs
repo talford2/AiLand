881ec36848d82c4bb1be71cf192ed5cbc3b1f0a2
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-27 01:35:55 +0800

    Add ArriveForce.

--- Assets/NPCs/Soldier/SoldierSteering.cs
+++ Assets/NPCs/Soldier/SoldierSteering.cs
@@ -11,4 +11,19 @@ public class SoldierSteering : BaseState<Soldier>
         var desiredVelocity = (position - NPC.transform.position).normalized*NPC.MaxSpeed;
         return desiredVelocity - NPC.Velocity;
     }
+
+    public Vector3 ArriveForce(Vector3 position)
+    {
+        var toPosition = position - NPC.transform.position;
+        var distance = toPosition.magnitude;
+        var deceleration = 0.6f;
+        if (distance > 0f)
+        {
+            var speed = distance / deceleration;
+            speed = Mathf.Min(speed, NPC.MaxSpeed);
+            var desiredVelocity = toPosition * speed / distance;
+            return desiredVelocity - NPC.Velocity;
+        }
+        return Vector3.zero;
+    }
 }
