fb9154653a601e99cc6f573567fa3f7dd1492bb1
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-27 01:54:28 +0800

    Flesh out Seek a little bit.

--- Assets/NPCs/Soldier/Soldier.cs
+++ Assets/NPCs/Soldier/Soldier.cs
@@ -6,6 +6,8 @@ public class Soldier : MonoBehaviour
 
 	public Animator AnimationController;
 
+    public SoldierSteering Steering { get; set; }
+
     public Vector3 Velocity { get; set; }
 
     public float MaxSpeed { get; set; }
--- Assets/NPCs/Soldier/SoldierSeek.cs
+++ Assets/NPCs/Soldier/SoldierSeek.cs
@@ -1,10 +1,40 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class SoldierSeek : BaseState<Soldier>
 {
+    private Vector3 destination;
+
 	public SoldierSeek(Soldier npc) : base(npc)
 	{
 		Debug.Log("Seek");
+	    NPC.MaxSpeed = 1f;
 	}
+
+    private Vector3 GetSteeringForce()
+    {
+        var sqrMaxSpeed = NPC.MaxSpeed*NPC.MaxSpeed;
+        var steerForce = Vector3.zero;
+
+        steerForce += NPC.Steering.ArriveForce(destination);
+        if (steerForce.sqrMagnitude > sqrMaxSpeed)
+            return NPC.MaxSpeed*steerForce.normalized;
+
+        steerForce += NPC.Steering.SeekForce(destination);
+        if (steerForce.sqrMagnitude > sqrMaxSpeed)
+            return NPC.MaxSpeed*steerForce.normalized;
+
+        return steerForce;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        NPC.Velocity += GetSteeringForce()*Time.deltaTime;
+        
+        // Locomotion
+        NPC.transform.rotation = Quaternion.LookRotation(destination - NPC.transform.position);
+        NPC.AnimationController.SetBool("IsAim", true);
+        NPC.AnimationController.SetFloat("Speed", NPC.Velocity.magnitude);
+    }
 }
