e12ca196890c95c3b824cbf529e644f4e3452ec2
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-27 23:38:53 +0800

    Improve chase locomotion.

--- Assets/NPCs/Soldier/SoldierChase.cs
+++ Assets/NPCs/Soldier/SoldierChase.cs
@@ -39,46 +39,53 @@ public class SoldierChase : BaseState<Soldier>
 		return steerForce;
 	}
 
-	public override void Update()
-	{
-		base.Update();
+    public override void Update()
+    {
+        base.Update();
 
-		if (chaseTarget != null && !NPC.IsDistanceGreaterThan(chaseTarget.position, NPC.ShootAttackRadius))
-		{
-			NPC.State = new SoldierShootAttack(NPC, chaseTarget);
-			return;
-		}
+        if (chaseTarget != null && !NPC.IsDistanceGreaterThan(chaseTarget.position, NPC.ShootAttackRadius))
+        {
+            NPC.State = new SoldierShootAttack(NPC, chaseTarget);
+            return;
+        }
 
-		npcPath.Update(chaseTarget.position);
-		useArriveForce = npcPath.IsFinalPathPoint();
+        npcPath.Update(chaseTarget.position);
+        useArriveForce = npcPath.IsFinalPathPoint();
 
-		NPC.Velocity += GetSteeringForce() * Time.deltaTime;
+        NPC.Velocity += GetSteeringForce()*Time.deltaTime;
 
-		// Locomotion
-		var targetForward = Utility.AtHeight(npcPath.GetCurrentPathTargetPosition(), 0f) - Utility.AtHeight(NPC.transform.position, 0f);
+        // Locomotion
+        Vector3 targetForward;
+        if (npcPath.IsFinalPathPoint())
+        {
+            targetForward = Utility.AtHeight(chaseTarget.position, 0f) - Utility.AtHeight(NPC.transform.position, 0f);
+        }
+        else
+        {
+            targetForward = Utility.AtHeight(npcPath.GetCurrentPathTargetPosition(), 0f) - Utility.AtHeight(NPC.transform.position, 0f);
+        }
 
-		NPC.transform.rotation = Quaternion.Lerp(NPC.transform.rotation, Quaternion.LookRotation(targetForward), Time.deltaTime);
-		NPC.AnimationController.SetBool("IsAim", false);
-		NPC.AnimationController.SetFloat("Speed", NPC.Velocity.magnitude);
+        NPC.transform.rotation = Quaternion.Lerp(NPC.transform.rotation, Quaternion.LookRotation(targetForward), 5f*Time.deltaTime);
+        NPC.AnimationController.SetBool("IsAim", false);
+        NPC.AnimationController.SetFloat("Speed", NPC.Velocity.magnitude);
 
-		NPC.AnimationController.SetFloat("HorizontalSpeed", Vector3.Dot(targetForward, NPC.transform.right));
-		NPC.AnimationController.SetFloat("VerticalSpeed", Vector3.Dot(targetForward, NPC.transform.forward));
+        NPC.AnimationController.SetFloat("HorizontalSpeed", Vector3.Dot(targetForward, NPC.transform.right));
+        NPC.AnimationController.SetFloat("VerticalSpeed", Vector3.Dot(targetForward, NPC.transform.forward));
 
-		npcPath.SetLastDestination(chaseTarget.position);
+        npcPath.SetLastDestination(chaseTarget.position);
 
+        // Target expires start seeking
+        targetExpirationCooldown -= Time.deltaTime;
+        if (targetExpirationCooldown <= 0)
+        {
+            var lastSeenPoint = chaseTarget.position;
+            chaseTarget = null;
+            NPC.State = new SoldierSeek(NPC, lastSeenPoint);
+        }
 
-		// Target expires start seeking
-		targetExpirationCooldown -= Time.deltaTime;
-		if (targetExpirationCooldown <= 0)
-		{
-			var lastSeenPoint = chaseTarget.position;
-			chaseTarget = null;
-			NPC.State = new SoldierSeek(NPC, lastSeenPoint);
-		}
+    }
 
-	}
-
-	public override void SeeTarget(Transform target)
+    public override void SeeTarget(Transform target)
 	{
 		targetExpirationCooldown = TargetExpirationTime;
 		chaseTarget = target;
