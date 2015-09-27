727e6c1f768d7bf4e8ee9cfa6687a8c63b035f64
talford2 <talford2@hotmail.com>
2015-09-28 01:53:43 +1000

    Wonder state

---
+++ Assets/NPCs/Soldier/SoldierWonder.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoldierWonder : BaseState<Soldier>
+{
+	public SoldierWonder(Soldier npc) : base(npc)
+	{
+		Debug.Log("Wonder");
+	}
+}
