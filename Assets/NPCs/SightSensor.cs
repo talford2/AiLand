018f9b2838d8f331550bdf5e816b6dc83f3e48f6
talford2 <talford2@hotmail.com>
2015-09-27 16:45:14 +1000

    Sight event

--- Assets/NPCs/SightSensor.cs
+++ Assets/NPCs/SightSensor.cs
@@ -1,32 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class SightSensor : MonoBehaviour
 {
-    public float Distance;
-    public float FieldOfView;
-    public float DetectFrequency;
+	public float Distance;
+	public float FieldOfView;
+	public float DetectFrequency;
 
-    private void Start()
-    {
-        StartCoroutine(Detect(1f));
-    }
+	public event Action<Transform> SeeTarget;
 
-    private IEnumerator Detect(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        Debug.Log("CHECK");
-        var detectedObjects = Physics.OverlapSphere(transform.position, Distance, LayerMask.GetMask("Detectable"));
-        foreach (var detected in detectedObjects)
-        {
-            Debug.Log("DETECTED: " + detected.name);
-        }
-        StartCoroutine(Detect(DetectFrequency));
-    }
+	private void Start()
+	{
+		StartCoroutine(Detect(1f));
+	}
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(transform.position, Distance);
-    }
+	private IEnumerator Detect(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		Debug.Log("CHECK");
+		var detectedObjects = Physics.OverlapSphere(transform.position, Distance, LayerMask.GetMask("Detectable"));
+		foreach (var detected in detectedObjects)
+		{
+			Debug.Log("DETECTED: " + detected.name);
+
+			if (SeeTarget != null)
+			{
+				SeeTarget(detected.transform);
+			}
+		}
+		StartCoroutine(Detect(DetectFrequency));
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.white;
+		Gizmos.DrawWireSphere(transform.position, Distance);
+	}
 }
--- Assets/NPCs/Soldier/Soldier.cs
+++ Assets/NPCs/Soldier/Soldier.cs
@@ -10,14 +10,26 @@ public class Soldier : MonoBehaviour
 
 	public Vector3 Velocity { get; set; }
 
+	public SightSensor sightSensor { get; set; }
+
 	public Transform Target;
 
 	public float MaxSpeed { get; set; }
 
-    void Awake()
-    {
-        Steering = new SoldierSteering(this);
-    }
+	void Awake()
+	{
+		Steering = new SoldierSteering(this);
+
+		sightSensor = GetComponent<SightSensor>();
+		sightSensor.SeeTarget += SeeTarget;
+	}
+
+	private void SeeTarget(Transform target)
+	{
+		Debug.Log("I saw something, I'm going to chase it");
+		Target = target;
+		State = new SoldierChase(this);
+	}
 
 	void Start()
 	{
