f333105f8e52589980fce62a0155625a64a1bf04
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-27 14:26:26 +0800

    Create SightSensor.

---
+++ Assets/NPCs/SightSensor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SightSensor : MonoBehaviour
+{
+    public float Distance;
+    public float FieldOfView;
+    public float DetectFrequency;
+
+    private void Start()
+    {
+        StartCoroutine(Detect(1f));
+    }
+
+    private IEnumerator Detect(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("CHECK");
+        var detectedObjects = Physics.OverlapSphere(transform.position, Distance, LayerMask.GetMask(""));
+        foreach (var detected in detectedObjects)
+        {
+        }
+        StartCoroutine(Detect(DetectFrequency));
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, Distance);
+    }
+}
