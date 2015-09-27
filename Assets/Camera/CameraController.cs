51459d58f7457ff50ddacf9192b9c941758a0aec
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-27 21:04:58 +0800

    Add CameraController.

---
+++ Assets/Camera/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 delta;
+
+    private void Update()
+    {
+        delta = 10f*new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+    }
+
+    private void LateUpdate()
+    {
+        transform.position += delta*Time.deltaTime;
+    }
+}
