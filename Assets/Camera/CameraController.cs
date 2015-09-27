19860183f3253f15cece5c75de880fd862a37729
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-27 22:25:06 +0800

    Add mouse wheel zoom to CameraController.

--- Assets/Camera/CameraController.cs
+++ Assets/Camera/CameraController.cs
@@ -3,14 +3,16 @@
 public class CameraController : MonoBehaviour
 {
     private Vector3 delta;
+    private float closeness;
 
     private void Update()
     {
         delta = 10f*new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        closeness = 100f*Input.GetAxis("Mouse ScrollWheel");
     }
 
     private void LateUpdate()
     {
-        transform.position += delta*Time.deltaTime;
+        transform.position += (delta + closeness*transform.forward)*Time.deltaTime;
     }
 }
