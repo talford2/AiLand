d51bcb7b92f389aed22cecb82327aa22b8063011
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-27 21:16:30 +0800

    Add TargetController and make target visible.

---
+++ Assets/Camera/TargetController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class TargetController : MonoBehaviour
+{
+    private Camera attachedCamera;
+    public Transform TargetObject;
+
+    private void Awake()
+    {
+        attachedCamera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            var mouseRay = attachedCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit mouseHit;
+            if (Physics.Raycast(mouseRay, out mouseHit, 100f, LayerMask.GetMask("Ground")))
+            {
+                TargetObject.position = mouseHit.point;
+            }
+        }
+    }
+}
