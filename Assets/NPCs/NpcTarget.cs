41191470cd664df3fdfed7a0e0039c3d9e2d0c32
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-28 23:17:37 +0800

    Add TargetingUtility/

---
+++ Assets/NPCs/NpcTarget.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class NpcTarget : MonoBehaviour
+{
+    public Team Team;
+
+    private void Awake()
+    {
+        TargetingUtility.AddTargetable(Team, transform);
+    }
+
+    private void OnDestroy()
+    {
+        TargetingUtility.RemoveTargetable(Team, transform);
+    }
+
+    public Team OpposingTeam
+    {
+        get
+        {
+            if (Team == Team.Good)
+                return Team.Bad;
+            return Team.Good;
+        }
+    }
+}
---
+++ Assets/NPCs/TargetingUtility.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TargetingUtility
+{
+    private static Dictionary<Team, List<Transform>> targetables;
+
+    public static void AddTargetable(Team team, Transform target)
+    {
+        if (targetables == null)
+            targetables = new Dictionary<Team, List<Transform>>();
+
+        if (!targetables.ContainsKey(team))
+            targetables.Add(team, new List<Transform>());
+        targetables[team].Add(target);
+    }
+
+    public static void RemoveTargetable(Team team, Transform target)
+    {
+        if (targetables.ContainsKey(team))
+            targetables[team].Remove(target);
+    }
+
+    public static Transform FindNearest(Team team, Vector3 fromPosition, float maxDistance)
+    {
+        Transform target = null;
+        if (targetables.ContainsKey(team))
+        {
+            var targetCandidates = targetables[team];
+            if (targetCandidates.Any())
+            {
+                var smallestDistanceSquared = maxDistance * maxDistance;
+                foreach (var candidate in targetCandidates)
+                {
+                    var toTarget = candidate.transform.position - fromPosition;
+                    if (toTarget.sqrMagnitude < smallestDistanceSquared)
+                    {
+                        smallestDistanceSquared = toTarget.sqrMagnitude;
+                        target = candidate;
+                    }
+                }
+            }
+        }
+        return target;
+    }
+}
+
+public enum Team
+{
+    Good,
+    Bad
+}
